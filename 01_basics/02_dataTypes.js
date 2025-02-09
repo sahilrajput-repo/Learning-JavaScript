d038e3342d25f1e9b0aaa8136b9632e8b3d7586c
Sahil Rajput <163540466+sahilrajput-repo@users.noreply.github.com>
2025-02-09 10:38:30 +0000

    dataTypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("Hello World") // we are using node js, not browser
+
+console.log(3 + 3)
+// code readability should be high
+console.log("Sahil Rajput")
+
+let name = "Sahil"
+let age = 27
+let isLoggedIn = true
+let state;
+
+// number => 2 to power 53
+// biging
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+// onject
+
+console.log(typeof undefined); // undefined
+
+console.log(typeof null); // null is an object
+
+ 
